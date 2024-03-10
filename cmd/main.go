78cb71acf53eca2313c1f2ffa74c134335c378a4
j3anvalj3an <j3anvalj3an@ruitunion.org>
2024-03-10 23:20:30 +0300

    healthcheck

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 
 	"github.com/hugin-and-munin/cred-checker/internal/app"
+	"github.com/hugin-and-munin/cred-checker/internal/config"
 	pb "github.com/hugin-and-munin/cred-checker/pb/github.com/hugin-and-munin/cred-checker"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", ":8080")
+
+	ServeHealthProbe()
+
+	port := fmt.Sprintf(":%s", config.GetValue(config.Port))
+
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -32,4 +39,15 @@ func NewCredCheckerServer() pb.CredCheckerServer {
 	httpClient := &http.Client{}
 
 	return app.NewCredChecker(httpClient)
+}
+
+func ServeHealthProbe() {
+	port := fmt.Sprintf(":%s", config.GetValue(config.HealthPort).String())
+	go func() {
+		log.Printf("Starting health check server on port %s", port)
+		log.Printf("Health check path: %s", config.GetValue(config.HealthPath).String())
+		http.ListenAndServe(port, nil)
+	}()
+
+	log.Printf("health probe listening at port %s", port)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"os"
+)
+
+type ConfigKey string
+
+var (
+	Port ConfigKey = "PORT"
+
+	HealthPath ConfigKey = "HEALTH_PATH"
+	HealthPort ConfigKey = "HEALTH_PORT"
+)
+
+type ConfigValue struct {
+	value any
+}
+
+var defaultValues map[ConfigKey]string = map[ConfigKey]string{
+	Port: "8080",
+
+	HealthPath: "healthy",
+	HealthPort: "8081",
+}
+
+func GetValue(key ConfigKey) ConfigValue {
+	value := os.Getenv(string(key))
+	if defaultValue, hasDefault := defaultValues[key]; len(value) == 0 && hasDefault {
+		value = defaultValue
+	}
+	return ConfigValue{
+		value: value,
+	}
+}
+
+func (v ConfigValue) String() string {
+	if val, ok := v.value.(string); ok {
+		return val
+	}
+	panic("ConfigValue is not string!")
+}
